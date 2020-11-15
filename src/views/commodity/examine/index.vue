b3cabcb3038e73818d645f72e5998d90944126d7
unknown <1728988727@qq.com>
2020-11-15 20:12:39 +0800

    12

---
+++ src/views/commodity/examine/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="mybody">
+    <div class="myheader">商品分类列表</div>
+    <el-table :data="tableData" stripe:true style="width: 100%">
+      <el-table-column prop="id" label="#"></el-table-column>
+      <el-table-column prop="classify" label="商品分类名称"></el-table-column>
+      <el-table-column
+        prop="classifyImage"
+        label="商品分类图片"
+      ></el-table-column>
+      <el-table-column fixed="right" label="操作" width="100">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small"
+            >查看</el-button
+          >
+          <el-button type="text" @click="modify(scope.row)" size="small"
+            >修改</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.mybody {
+  padding: 20px;
+}
+.myheader {
+  align-self: start;
+  font-size: 24px;
+  margin: 20px 0px;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          id: "3",
+          classify: "女装",
+          classifyImage: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          id: "4",
+          classify: "女装",
+          classifyImage: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          id: "5",
+          classify: "女装",
+          classifyImage: "上海市普陀区金沙江路 1518 弄",
+        },
+      ],
+    };
+  },
+  methods: {
+    handleClick(data) {
+      alert("查看" + data.classify);
+    },
+    modify(data) {
+      this.$router.push({
+        name: "Modify",
+        params: {
+          data: data
+        },
+      });
+    },
+  },
+};
+</script>
---
+++ src/views/commodity/modify/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="mybody">
+    <div class="myheader">添加商品分类</div>
+    <el-form ref="form" :model="form" label-width="230px">
+      <el-form-item label="商品分类名称">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="商品分类图片(图片规格：171*180)">
+        <el-upload
+          class="upload-demo"
+          action="#"
+          :on-preview="handlePreview"
+          :on-remove="handleRemove"
+          :before-remove="beforeRemove"
+          multiple
+          :limit="3"
+          :on-exceed="handleExceed"
+        >
+          <el-button size="small" type="primary">点击上传</el-button>
+          <div slot="tip" class="el-upload__tip">
+            只能上传jpg/png文件，且不超过500kb
+          </div>
+        </el-upload>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">立即创建</el-button>
+        <el-button>取消</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<style scoped>
+.mybody {
+  padding: 20px;
+}
+.myheader {
+  align-self: start;
+  font-size: 24px;
+  margin: 20px 0px;
+}
+</style>
+
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: "",
+      }
+    };
+  },
+  methods: {
+    onSubmit() {
+      alert("·添加商品分类成功")
+    },
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePreview(file) {
+      console.log(file);
+    },
+    handleExceed(files, fileList) {
+      this.$message.warning(
+        `当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${
+          files.length + fileList.length
+        } 个文件`
+      );
+    },
+    beforeRemove(file, fileList) {
+      return this.$confirm(`确定移除 ${file.name}？`);
+    },
+  },
+};
+</script>
